91380efe01124863472976cbca6cc4ec91f54d22
wujincun <1769453779@qq.com>
2016-10-18 09:52:27 +0800

    webpack'学习1、babel +webpack

--- es6/index.js
+++ es6/index.js
@@ -3,12 +3,20 @@
  */
 var fn = () =>{
     console.log(123)
-}
+};
 fn();
-class Student(){
+
+class Student{
     hello(){
         console.log("hello")
     }
 }
 var s = new Student;
 s.hello();
+
+class leo extends Student{
+
+}
+var l = new leo;
+l.hello();
+
